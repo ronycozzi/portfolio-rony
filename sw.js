ad7dc961ab34b94b09729124305175a653d71a0a
Rony Cozzi <ronycozzi5@gmail.com>
2026-06-18 17:31:04 -0300

    Refine portfolio copy and cache bust main bundle

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Service Worker - Rony Cozzi Portfolio */
-const VERSION = 'v67';
+const VERSION = 'v68';
 const PRECACHE = `rony-portfolio-precache-${VERSION}`;
 const RUNTIME = `rony-portfolio-runtime-${VERSION}`;
 const MAX_RUNTIME_ENTRIES = 60;
@@ -37,7 +37,7 @@ const SHELL = [
   '/css/case.css',
   '/css/case.css?v=3',
   '/js/main.js',
-  '/js/main.js?v=43',
+  '/js/main.js?v=44',
   '/manifest.json',
   '/favicon.svg',
   '/og-image.png',
