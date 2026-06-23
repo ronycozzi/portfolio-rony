3596715eea3d8fedebc8bab087f9f2c0a1df3ff0
Rony Cozzi <ronycozzi5@gmail.com>
2026-06-23 15:50:45 -0300

    Fix static prefetch audit noise

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Service Worker - Rony Cozzi Portfolio */
-const VERSION = 'v80';
+const VERSION = 'v81';
 const PRECACHE = `rony-portfolio-precache-${VERSION}`;
 const RUNTIME = `rony-portfolio-runtime-${VERSION}`;
 const MAX_RUNTIME_ENTRIES = 60;
